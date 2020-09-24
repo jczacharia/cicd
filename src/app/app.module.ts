7a39f2c0d39e999569809bf346c0e53cfa0ac9cf
Jeremy Zacharia <jczacharia@gmail.com>
2020-09-24 12:27:12 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyAOEEQj5uV-CRU-pBwgbSdUKefcKfgJ2Bo',
+      authDomain: 'cicd-4bcc4.firebaseapp.com',
+      databaseURL: 'https://cicd-4bcc4.firebaseio.com',
+      projectId: 'cicd-4bcc4',
+      storageBucket: 'cicd-4bcc4.appspot.com',
+      messagingSenderId: '694345962459',
+      appId: '1:694345962459:web:33a084c0047aec6164cdfe',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
